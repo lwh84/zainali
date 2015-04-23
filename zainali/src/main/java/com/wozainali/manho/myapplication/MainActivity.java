d02a40aa26f8bd284e07f091cb92de77b95638e1
lwh84 <luowenhao84@gmail.com>
2015-04-24 01:03:38 +0200

    rename some files #4

--- zainali/src/main/java/com/wozainali/manho/myapplication/MainActivity.java
+++ zainali/src/main/java/com/wozainali/manho/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
