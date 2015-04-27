3cba4b261b6cb0a418a6c0b80a16a464d9ed7bb0
lwh84 <luowenhao84@gmail.com>
2015-04-28 00:29:14 +0200

    add comment and ideas of pointchecker #30

---
+++ zainali/src/main/java/com/wozainali/manho/myapplication/asynctasks/ShowCountryNameAndBorder.java
@@ -0,0 +1,39 @@
+package com.wozainali.manho.myapplication.asynctasks;
+
+public class ShowCountryNameAndBorder {
+
+    public void showCountryNameAndBorder(double longitude, double latitude) {
+
+        // get the list
+
+        // for every placemark check min max lat long
+
+        // if longitude < min long return
+        // if longitude > max long return
+        // if latitude < min lat return
+        // if longitude > max lat return
+        // if still here
+        // get polygons
+        // for every polygon, make polygon object Double.
+        // Path2D path = new Path2D.Double();
+//        path.moveTo(valoresX[0], valoresY[0]);
+//        for(int i = 1; i < valoresX.length; ++i) {
+//            path.lineTo(valoresX[i], valoresY[i]);
+//        }
+//        path.closePath();
+        //
+        // path.contains(points) then check point in polygon.
+
+        // when placemark found getname and getpolygons, get min and max lat and long
+
+        // send event to zoom in and animate to point
+
+        // send event to addmarker on that point with name
+
+        // send events to draw polylines
+
+
+
+    }
+
+}
